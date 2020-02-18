6e31a0c3d40e967a2c6bcb8122ecc1608ad47e8e
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 19:11:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,17 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-
+ new_hash = {}
+ dir_index = 0
+  while dir_index < nds.length do 
+  dir_name = nds[dir_index][:name]
+  new_hash[dir_name] = 0
+  i = 0
+  while i < nds[dir_index][:movies].length do
+    new_hash[dir_name] += nds[dir_index][:movies][i][:worldwide_gross]
+    i+=1
+  end
+  dir_index +=1
+end
+new_hash
 end
