750a59ce6cc11d4a780aa2d86d5e182232df47d6
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 19:23:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,12 +11,22 @@ end
 dir_total 
 end
 
+def gross_for_director(nds)
+dir_total = 0 
+i = 0 #movies 
+while i < nds[:movies].length do
+dir_total += nds[:movies][i][:worldwide_gross]  
+i+=1
+end 
+dir_total 
+end
+
 def directors_totals(nds)
  new_hash = {}
  dir_index = 0
   while dir_index < nds.length do 
   dir_name = nds[dir_index][:name]
-  new_hash[dir_name] = gross_for_director(dir_index)
+  new_hash[dir_name] = gross_for_director(nds)
   dir_index +=1
 end
 new_hash
